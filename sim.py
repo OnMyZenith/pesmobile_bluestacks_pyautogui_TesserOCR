8dff0174a43173db233acb5d3c199230d991bc8b
OnMyZenith <asr3187@gmail.com>
2021-07-31 13:29:55 +0530

    Update sim.py

--- sim.py
+++ sim.py
@@ -479,8 +479,9 @@ def identifyOneHalfOfScoutsOnPage(allSkills_or_Categories, isCategory):
     oneHalfOfThreeScouts=[]
     for i in range(3):
         foundOneHalfOfOneScout = None
+        foundCategory = None
+
         if isCategory:
-            foundCategory = None
             for j in allSkills_or_Categories:
                 if(foundCategory:=checkInRow(j, i)):
                     break
