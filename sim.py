4edc304dc5566b7634f84b87278d4ea5ec7d44e6
OnMyZenith <asr3187@gmail.com>
2021-08-01 20:18:24 +0530

    Update sim.py

--- sim.py
+++ sim.py
@@ -441,16 +441,16 @@ def autosim():
         if check(ToMatch):
             matchNumber += 1
             changedMatchNumber = True
-            pyg.screenshot('./logs/MatchScreenshots/'+folderName+'MatchNumber_'+str(matchNumber)+'_1.png')
+            pyg.screenshot('./logs/MatchScreenshots/'+folderName+' Match Number '+str(matchNumber)+'_1.png')
             print2Both("Took a Screenshot of Campaign Summary")
             click(ToMatch, False, 15 + waitLong)     #Wait 30 secs for SNF and OK to show up if thery're going to for Matchmaking to Finish
-            pyg.screenshot('./logs/MatchScreenshots/'+folderName+'MatchNumber_'+str(matchNumber)+'_2.png')
+            pyg.screenshot('./logs/MatchScreenshots/'+folderName+' Match Number_'+str(matchNumber)+'_2.png')
             print2Both("Took a Screenshot of Matchmaking")
 
         if check(SquadNotFine):
             if changedMatchNumber:
-                os.remove('./logs/MatchScreenshots/'+folderName+'MatchNumber_'+str(matchNumber)+'_1.png')
-                os.remove('./logs/MatchScreenshots/'+folderName+'MatchNumber_'+str(matchNumber)+'_2.png')
+                os.remove('./logs/MatchScreenshots/'+folderName+' Match Number_'+str(matchNumber)+'_1.png')
+                os.remove('./logs/MatchScreenshots/'+folderName+' Match Number_'+str(matchNumber)+'_2.png')
                 matchNumber -= 1
             if(check(ok)):
                 click(ok, True, waitLong)
